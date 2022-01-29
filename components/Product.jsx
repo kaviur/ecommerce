8fb508ff8c90a17c94b24006f6e723b7109718a7
Karen <quemojojojo@hotmail.com>
2022-01-29 13:37:04 -0500

    aplicando estilos en listado de productos

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export default function Product({product}) {
-  return <article>
-            <h4>{product.name}</h4>
+  return <article className='bg-slate-300'>
+            <h3>{product.name}</h3>
             <p>{product.description}</p>
             <img src={product.img}></img>
         </article>;
