e5b9e590a2d99199cd080f3817ba461a7b3f9963
Karen <quemojojojo@hotmail.com>
2022-01-29 15:04:40 -0500

    comenté acceso a credenciales firebase

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export default function Product({product}) {
-  return <article className='bg-slate-300'>
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
+  return <article className='bg-slate-300 shadow-md'>
             <img src={product.img}></img>
+            <div className='p-2'>
+              <h3 className='text-2xl font-bold'>{product.name}</h3>
+              <p>{product.description}</p>
+            </div>
         </article>;
 }
 
--- database/index.js
+++ database/index.js
@@ -1,15 +1,15 @@
-import admin from 'firebase-admin'
-import serviceAccount from './credentials.json'
+// import admin from 'firebase-admin'
+// import serviceAccount from './credentials.json'
 
-let app
+// let app
 
-if(!admin.apps.length){
-    app = admin.initializeApp({
-        credential: admin.credential.cert(serviceAccount)
-    });
-}else{
-    app = admin.app()
-}
-const database = app.firestore()
+// if(!admin.apps.length){
+//     app = admin.initializeApp({
+//         credential: admin.credential.cert(serviceAccount)
+//     });
+// }else{
+//     app = admin.app()
+// }
+// const database = app.firestore()
 
-export default database
+// export default database
