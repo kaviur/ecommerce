455c700ef0328bbf517a277e65e23b49e67efcdc
Clara-V-B <83819114+Clara-V-B@users.noreply.github.com>
2022-02-05 19:02:34 -0300

    actualización navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,43 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
+import { useSelector } from 'react-redux';
+
+export default function Navbar() {
+  const [show, setShow] = useState(false);
+  const { items: cart } = useSelector((state) => state.cart);
+
+  return (
+    <nav className="px-5 py-3 mb-5 bg-white shadow-md sm:px-0">
+      <div className="flex justify-between max-w-4xl mx-auto">
+        <Link href={'/'}>Inicio</Link>
+
+        <ul className={`gap-5 ${show ? 'flex' : 'hidden'} sm:flex`}>
+          <li>
+            <Link href="/productos">Productos</Link>
+          </li>
+          <li>
+            <Link href="/productos">Productos</Link>
+          </li>
+          <li>
+            <Link href="/cart" passHref>
+              <p>Carrito {cart.length}</p>
+            </Link>
+          </li>
+        </ul>
+        <button
+          className="sm:hidden"
+          onClick={() => {
+            setShow(!show);
+          }}
+        >
+          Menu
+        </button>
+      </div>
+    </nav>
+  );
+}
+/*import React, { useState } from 'react';
+import Link from 'next/link';
 
 export default function Navbar() {
   const [menu,setMenu] = useState(true)
@@ -17,4 +55,4 @@ export default function Navbar() {
         <button className='sm:hidden' onClick={()=>setMenu(!menu)}>Menu</button>
       </div>
   </nav>;
-}
+}*/
